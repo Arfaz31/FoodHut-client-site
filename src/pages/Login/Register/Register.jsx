95858b48879db64c93b23a869aaae1fd4c19cb57
ArfazAhamed <arfazahamed31@gmail.com>
2023-05-04 23:44:03 +0600

    registration data get and validation data

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../providers/AuthProvider';
 
 const Register = () => {
 const {createUser} = useContext(AuthContext)
+const [success, setSuccess] = useState('')
+const[error, setError] = useState('')
 
 const handleRegister = event => {
     event.preventDefault()
@@ -13,12 +15,21 @@ const handleRegister = event => {
     const email = form.email.value;
     const password = form.password.value
     const  photo = form.photo.value
+
+    // validation
+    if(password.length < 6){
+        setError('please add at least 6 characters');
+        return;
+    }
     createUser(email, password)
     .then(result => {
         const createdUser = result.user
+        console.log(createdUser);
+        setSuccess('User has created successfully')
     })
     .catch(error => {
-        
+        console.error(error.message)
+        setError(error.message)
     })
 }
 
@@ -40,6 +51,9 @@ const handleRegister = event => {
     <Form.Label>Password</Form.Label>
     <Form.Control type="password" name='password' placeholder="Password" required />
   </Form.Group>
+  <Form.Text className="text-danger mb-2">
+      {error}
+      </Form.Text>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Photo URL</Form.Label>
     <Form.Control type="text" name='photo' placeholder="Photo URL" required />
@@ -56,13 +70,12 @@ const handleRegister = event => {
   <Form.Text className="text-white">Don't have an account?
       <Link to="/login">  Login</Link>
       </Form.Text>
-
-  <Form.Text className="text-success">
-      
+<br />
+  <Form.Text className="text-info">
+      {success}
       </Form.Text>
-      <Form.Text className="text-danger">
+      <br />
       
-      </Form.Text>
 </Form> 
         </div>
        </div>
